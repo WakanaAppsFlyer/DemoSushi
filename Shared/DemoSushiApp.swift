ab7957c4bf2d94821cd5ee86b80ad35e9960692a
wknar <wakana.ara@gmail.com>
2020-09-07 16:15:55 +0900

    Initial Commit

---
+++ Shared/DemoSushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSushiApp.swift
+//  Shared
+//
+//  Created by Wakana Ara on 2020/09/07.
+//
+
+import SwiftUI
+
+@main
+struct DemoSushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
